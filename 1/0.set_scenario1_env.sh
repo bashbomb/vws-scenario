da7e63061ae2ab4eb9213ba54bdacf68bfc1ee7c
kang.minkoo <kang.minkoo@colon.ne.jp>
2025-04-20 19:12:59 +0900

    Fix bug

--- 1/0.set_scenario1_env.sh
+++ 1/0.set_scenario1_env.sh
@@ -8,16 +8,16 @@ mkdir -p /mnt/backup/logs
 # 2. 샘플 로그 삽입
 TODAY=$(date '+%b %d')
 DATE=$(date '+%d/%b/%Y:%H:%M:%S %z')
-echo {
-${TODAY} 00:00:01 localhost kernel: [    0.000000] Initializing cgroup subsys cpuset
-${TODAY} 00:01:23 localhost systemd[1]: Started Daily Cleanup of Temporary Directories.
-${TODAY} 00:02:45 localhost sshd[1234]: error: PAM: Authentication failure for illegal user admin from 192.168.0.100
+{
+echo "${TODAY} 00:00:01 localhost kernel: [    0.000000] Initializing cgroup subsys cpuset"
+echo "${TODAY} 00:01:23 localhost systemd[1]: Started Daily Cleanup of Temporary Directories."
+echo "${TODAY} 00:02:45 localhost sshd[1234]: error: PAM: Authentication failure for illegal user admin from 192.168.0.100"
 } >> /var/log/messages
-echo {
-127.0.0.1 - - [${DATE}] "GET /index.html HTTP/1.1" 200 1024
-127.0.0.1 - - [${DATE}] "POST /login HTTP/1.1" 302 512
-192.168.0.5 - - [${DATE}] "GET /dashboard HTTP/1.1" 200 2048
-}  >> /var/log/nginx/access.log
+{
+  echo '127.0.0.1 - - ['"${DATE}"'] "GET /index.html HTTP/1.1" 200 1024'
+  echo '127.0.0.1 - - ['"${DATE}"'] "POST /login HTTP/1.1" 302 512'
+  echo '192.168.0.5 - - ['"${DATE}"'] "GET /dashboard HTTP/1.1" 200 2048'
+} >> /var/log/nginx/access.log
 echo "${TODAY} 00:00:01 localhost CROND[2222]: (root) CMD (/usr/local/bin/backup.sh)" >> /var/log/cron
 
 # 3. 백업 스크립트 배치

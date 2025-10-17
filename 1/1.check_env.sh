8427ec35db0dd42b9183998c02328632dada703b
kang.minkoo <kang.minkoo@colon.ne.jp>
2025-10-17 17:58:20 +0900

    Fix bug

--- 1/1.check_env.sh
+++ 1/1.check_env.sh
@@ -10,12 +10,13 @@ grep -q 'error' /var/log/messages || errors=$((errors+1))
 [ -x /usr/local/bin/backup.sh ] || errors=$((errors+1))
 crontab -l | grep -q '/usr/local/bin/backup.sh' || errors=$((errors+1))
 grep -q 'PATH=/bin' /etc/cron.d/0hourly || errors=$((errors+1))
+pgrep crond || errors=$((errors+1))
 
 # 결과 출력
-if [ "$errors" -eq 0 ]; then
+if [ "${errors}" -eq 0 ]; then
   echo "실습 환경이 정상적으로 설정되었습니다. 시나리오를 진행하세요."
 else
   echo "실습 환경 설정에 문제가 있습니다."
   echo "컨테이너를 재시작한 뒤, 다시 다음 명령어를 실행하세요:"
-  echo "./scenario1-1.sh"
+  echo "./0.set_scenario1_env.sh"
 fi

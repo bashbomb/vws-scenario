f9855e8d25a6e26b8307dfc964f5924aaf307153
kang.minkoo <kang.minkoo@colon.ne.jp>
2025-10-17 17:59:46 +0900

    Fix bug

--- 1/1.check_env.sh
+++ 1/1.check_env.sh
@@ -10,7 +10,7 @@ grep -q 'error' /var/log/messages || errors=$((errors+1))
 [ -x /usr/local/bin/backup.sh ] || errors=$((errors+1))
 crontab -l | grep -q '/usr/local/bin/backup.sh' || errors=$((errors+1))
 grep -q 'PATH=/bin' /etc/cron.d/0hourly || errors=$((errors+1))
-pgrep crond || errors=$((errors+1))
+pgrep crond /dev/null 2>&1 || errors=$((errors+1))
 
 # 결과 출력
 if [ "${errors}" -eq 0 ]; then
